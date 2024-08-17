6d39b6f33243d460e2038d9b19cde5b01e2f3779
Rony Carrillo <ronycarrillobermudez@gmail.com>
2024-08-17 12:48:10 -0600

    Obtener usuario por carne, obetner usuario por ID

--- src/main/java/umg/progra2/Main.java
+++ src/main/java/umg/progra2/Main.java
@@ -7,11 +7,99 @@ import umg.progra2.Creds.credencialesBot;
 import umg.progra2.botTelegram.Bot;
 import umg.progra2.botTelegram.PokemonBot;
 import umg.progra2.botTelegram.tareaBot;
+import umg.progra2.model.User;
+import umg.progra2.service.UserService;
+
+import java.sql.SQLException;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static void PruebaInsertaUsuario() {
+        //explicación:
+        // 1. Servicio
+        //Servicio (UserService.java):
+        //La clase UserService actúa como intermediario entre el controlador y la capa de acceso a datos (DAO).
+        // Se encarga de la lógica de negocio, validaciones y de coordinar las transacciones.
+        // 2. DAO
+        //Capa de Acceso a Datos (UserDao.java):
+        //Esta clase contiene los métodos para interactuar con la base de datos, usando la
+        // conexión proporcionada por DatabaseConnection. Aquí es donde se construyen y ejecutan
+        // las consultas SQL.
+        // 3. Conexión a la Base de Datos
+        //Gestión de la Conexión (DatabaseConnection.java):
+        //Esta clase es responsable de proporcionar la conexión a la base de datos. Puede leer la configuración
+        // desde un archivo de propiedades (application.properties) para obtener los detalles de conexión.
+        // 4. Transacciones
+        //Gestión de Transacciones (TransactionManager.java):
+        //Esta clase se encarga de iniciar, confirmar o revertir transacciones en la base de datos.
+        // Se utiliza para agrupar varias operaciones en una sola transacción y garantizar la integridad de los datos.
+        // 5. Modelo (User.java):
+        //La clase User representa la estructura de los datos que se insertan en la base de datos.
+        // Es una clase POJO (Plain Old Java Object) con atributos, getters y setters.
+
+        //invoca el servicio que manejará la lógica de negocio.
+        UserService userService=new UserService();
+        User user = new User();
+
+        // Crear un nuevo usuarioUseruser=newUser();
+        user.setCarne("0905-12-12345");
+        user.setNombre("Andrea Lopez");
+        user.setCorreo("ALopez@gmail.com");
+        user.setSeccion("A");
+        user.setTelegramid(1234567890L);
+        user.setActivo("Y");
+
+        try {
+            userService.createUser(user);
+            System.out.println("User created successfully!");
+        } catch (SQLException e) {
+            System.out.println("hay clavos!!");
+            e.printStackTrace();
+        }
+    }
+
+    private static void PruebaActualizacionUsuario() {
+        UserService servicioUsuaio = new UserService();
+
+        User usurioObtenido;
+        //obtener información del usuario por correo electrónico
+        try {
+            //usurioObtenido = servicioUsuaio.getUserByEmail("ALopez@gmail.com");
+            usurioObtenido = servicioUsuaio.getUserBycarne("0905-21-15472");
+            //usurioObtenido = servicioUsuaio.getUserById(2);
+            System.out.println("Retrieved User: " + usurioObtenido.getNombre());
+            System.out.println("Retrieved User: " + usurioObtenido.getCorreo());
+            System.out.println("Retrieved User: " + usurioObtenido.getId());
+
+            //actualizar información del usuario
+            usurioObtenido.setCarne("0905-21-15472");
+            usurioObtenido.setNombre("Rony Osman Carrillo Bermudez");
+            usurioObtenido.setCorreo("ronycarrillobermudez@gmail.com");
+            usurioObtenido.setSeccion("A");
+            usurioObtenido.setTelegramid(1232386900);
+            usurioObtenido.setActivo("Y");
+
+            servicioUsuaio.updateUser(usurioObtenido);
+            System.out.println("User updated successfully!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void PruebaEliminarUsuario() {
+        UserService servicioUsuaio = new UserService();
+        try {
+            servicioUsuaio.deleteUserByEmail("anAscoli@gmail.com");
+            System.out.println("User deleted successfully!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
+        PruebaActualizacionUsuario();
+        //PruebaInsertaUsuario();
 
         try {
             TelegramBotsApi botApi = new TelegramBotsApi(DefaultBotSession.class);
@@ -23,13 +111,12 @@ public class Main {
 
             tareaBot hmBot = new tareaBot();
             botApi.registerBot(hmBot);
+
+
             System.out.println("El bot se est[a ejecutando!!!");
 
         }catch (Exception ex){
             System.out.println("Error al instanciar Telegram: " + ex.getMessage());
         }
-
-
-
     }
 }
