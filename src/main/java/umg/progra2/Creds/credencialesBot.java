e5ab2665aa917e1fdb917ecf4abe8225a56a4b5f
Rony Carrillo <ronycarrillobermudez@gmail.com>
2024-08-17 11:22:09 -0600

    Informacio del estudiante, cambio de crendenciales, opinion de progra, hola y cambio.

---
+++ src/main/java/umg/progra2/Creds/credencialesBot.java
@@ -0,0 +1,18 @@
+package umg.progra2.Creds;
+
+public class credencialesBot {
+
+    String nombre_bot = "Wayn3bot";
+    String token_bot = "6351827444:AAF77BQw9wPvL3prcAqbT7rGFttDfOs3__Q";
+
+    public String name_bot_request(){
+        return nombre_bot;
+    }
+
+    public String token_bot_request(){
+        return token_bot;
+    }
+}
+
+
+
